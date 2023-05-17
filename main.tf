380fd43a6db45ca7ee51112fb562f39108433098
Brandon-Arbour <brandon-arbour@mohawkcollege.ca>
2023-05-17 10:27:27 -0400

    Fixing VPC configuration

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -35,6 +36,8 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = local.subnet_id
+  associate_public_ip_address = true
   tags = {
     Name = "Learning Terraform"
   }
