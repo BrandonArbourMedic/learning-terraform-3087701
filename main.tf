7c2d281e38b7b180dcdb66ec1ea2ac157125d708
Brandon-Arbour <brandon-arbour@mohawkcollege.ca>
2023-05-17 10:52:09 -0400

    Fixing VPC subnet

--- main.tf
+++ main.tf
@@ -35,9 +35,7 @@ resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = local.subnet_id
-  associate_public_ip_address = true
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "Learning Terraform"
   }
