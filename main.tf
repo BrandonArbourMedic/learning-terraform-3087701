0c1db91748c35ae027b0d83737b1de16d938e8de
Brandon-Arbour <brandon-arbour@mohawkcollege.ca>
2023-05-17 14:23:52 -0400

    Created autoscaling, change load balancer name

--- main.tf
+++ main.tf
@@ -31,17 +31,22 @@ module "blog_vpc" {
     Environment = "dev"
   }
 }
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "Learning Terraform"
-  }
+
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  
+  name = "blog"
+ 
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  image_id            = data.aws_ami.app_ami.id
+  security_groups     = [module.blog_sg.security_group_id]
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -49,9 +54,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -62,7 +67,7 @@ module "alb" {
       targets = {
         my_target = {
           target_id = aws_instance.blog.id
-          port = 80
+          port      = 80
         }
       }
     }
@@ -88,8 +93,8 @@ module "blog_sg" {
   
   vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["142.222.16.0/24"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["142.222.16.0/24"]
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = ["142.222.16.0/24"]
 }
